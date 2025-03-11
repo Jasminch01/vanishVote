b95dbd01abd5e4dfe4551befca94882007c78871
jasmin <chjasmin27@gmail.com>
2025-03-11 08:53:54 +0600

    debug dontenv config

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 const app = express();
 dotenv.config({ path: path.join(process.cwd(), ".env") });
 const port = 5000;
